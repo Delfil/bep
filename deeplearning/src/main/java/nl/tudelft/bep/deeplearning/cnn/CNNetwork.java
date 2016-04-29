6ddc75eccdcd77cff794d0401558cf82d61b1355
S. Smulders <s.smulders@student.tudelft.nl>
2016-04-29 16:07:42 +0200

    CNN to simple NN

--- deeplearning/src/main/java/nl/tudelft/bep/deeplearning/cnn/CNNetwork.java
+++ deeplearning/src/main/java/nl/tudelft/bep/deeplearning/cnn/CNNetwork.java
@@ -8,7 +8,6 @@ import org.deeplearning4j.nn.conf.Updater;
 import org.deeplearning4j.nn.conf.layers.ConvolutionLayer;
 import org.deeplearning4j.nn.conf.layers.DenseLayer;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
-import org.deeplearning4j.nn.conf.layers.SubsamplingLayer;
 import org.deeplearning4j.nn.conf.layers.setup.ConvolutionLayerSetup;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
@@ -40,66 +39,39 @@ public class CNNetwork {
 		
 		log.info("Load data....");
 		
-		String fileName = "/100data";
-		MatrixDataFetcher fetcher = new MatrixDataFetcher(fileName, false, seed, width, height);
+		String fileName = "/allData";
+		MatrixDataFetcher fetcher = new MatrixDataFetcher(fileName, false, seed, width, height, true, 0.6);
 		DataSetIterator mnistTrain = new MatrixDatasetIterator(batchSize,
 				fetcher);
 		DataSetIterator mnistTest = new MatrixDatasetIterator(batchSize,
-				new MatrixDataFetcher(fileName, false, seed, width, height));
+				new MatrixDataFetcher(fileName, false, seed, width, height, false, 0.6));
 
 		int outputNum = fetcher.getOutputNum();
-		
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 		
 		log.info("Build model....");
 		MultiLayerConfiguration.Builder builder = new NeuralNetConfiguration.Builder()
-                .seed(seed)
-                .iterations(1)
-                .regularization(true).l2(0.0005)
-                .learningRate(0.01)
-                .weightInit(WeightInit.XAVIER)
-                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-                .updater(Updater.NESTEROVS).momentum(0.9)
-                .list(6)
-                .layer(0, new ConvolutionLayer.Builder(5, 5)
-                        .nIn(1)
-                        .stride(1, 1)
-                        .nOut(20)
-                        .activation("identity")
-                        .build())
-                .layer(1, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
-                        .kernelSize(2,2)
-                        .stride(2,2)
-                        .build())
-                .layer(2, new ConvolutionLayer.Builder(5, 5)
-                        .nIn(1)
-                        .stride(1, 1)
-                        .nOut(50)
-                        .activation("identity")
-                        .build())
-                .layer(3, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
-                        .kernelSize(2,2)
-                        .stride(2,2)
-                        .build())
-                .layer(4, new DenseLayer.Builder()
-                		.activation("relu")
-                        .nOut(50).build())
-                .layer(5, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
-                        .nOut(outputNum)
-                        .activation("softmax")
-                        .build())
-                .backprop(true).pretrain(false);
-        new ConvolutionLayerSetup(builder,width,height,1);
+				.seed(seed)
+				.iterations(1)
+				.regularization(true).l2(0.0005)
+				.learningRate(0.01)
+				.weightInit(WeightInit.XAVIER)
+				.optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+				.updater(Updater.NESTEROVS).momentum(0.9)
+				.list(3)
+				.layer(0, new ConvolutionLayer.Builder(1, 1)
+						.stride(1, 1)
+						.nOut(1)
+						.activation("identity")
+						.build())
+				.layer(1, new DenseLayer.Builder()
+						.nOut(50).build())
+				.layer(2, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+						.nOut(outputNum)
+						.activation("softmax")
+						.build())
+				.backprop(true).pretrain(false);
+		new ConvolutionLayerSetup(builder,width,height,1);
 
 		MultiLayerConfiguration conf = builder.build();
 		MultiLayerNetwork model = new MultiLayerNetwork(conf);
