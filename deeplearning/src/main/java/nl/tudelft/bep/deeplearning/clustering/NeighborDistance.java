69369deb8d3cea8b3d00c77c9e441be9204ac8cd
paulbakker65 <P.Bakker-2@student.tudelft.nl>
2016-05-10 13:07:39 +0200

    Added javadoc getPointID

--- deeplearning/src/main/java/nl/tudelft/bep/deeplearning/clustering/NeighborDistance.java
+++ deeplearning/src/main/java/nl/tudelft/bep/deeplearning/clustering/NeighborDistance.java
@@ -19,11 +19,16 @@ public class NeighborDistance {
 		return distances;
 	}
 	
-	public int getPointIDWithClosestNeighbor(int no) {
+	/**
+	 * Gets the nth point where the points are sorted on the distance to its nearest neighbor
+	 * @param n the nth point.
+	 * @return the nth point's ID where the points are sorted on distance to neighbor.
+	 */
+	public int getPointIDWithClosestNeighbor(int n) {
 		Double[] tempDist = distances.clone();
 		double temp = Double.MAX_VALUE;
 		int index = -1;
-		for(int c = 0; c < no; c++) {
+		for(int c = 0; c < n; c++) {
 			index = -1;
 			temp = Double.MAX_VALUE;
 			for(int i = 0; i < tempDist.length; i++) {
