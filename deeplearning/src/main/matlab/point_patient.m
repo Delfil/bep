04b319bee397b346d98c96ea9b68e51f2dbd140b
paulbakker65 <P.Bakker-2@student.tudelft.nl>
2016-05-12 17:00:14 +0200

    Add comments matlab file

--- deeplearning/src/main/matlab/point_patient.m
+++ deeplearning/src/main/matlab/point_patient.m
@@ -1,7 +1,10 @@
+%File of points we would like to create
 file_points = fopen('100points.in','w');
 
+%Number of points printed
 fprintf(file_points,'%i\n', size(P,1));
 
+%Get all the points and print on each line
 for i = 1:size(P,1)
     fprintf(file_points,'%f ', P(i,1:end-1));
     fprintf(file_points,'%f\n', P(i,end));
@@ -9,9 +12,12 @@ end
 
 fclose(file_points);
 
+%File of points we would like to create
 file_patients = fopen('geneact.in','w');
 
+%Print the number of labels as first line
 fprintf(file_patients, '%i\n', size(CancerTypeList,1));
+%Get all the gene information into the file
 for i = 1:size(GE,1)
     fprintf(file_patients,'%f ', GE(i,1:end-1));
     fprintf(file_patients,'%f\n', GE(i,end));
