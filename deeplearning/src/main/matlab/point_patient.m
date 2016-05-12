7e1fee56578c67fa56e39b4f660e56d8921d15b6
paulbakker65 <P.Bakker-2@student.tudelft.nl>
2016-05-12 17:02:33 +0200

    Revert "Revert "Revert "Add comments matlab file"""
    This reverts commit 11215138838ad7bbd750a4129428c6301e8f4291.

--- deeplearning/src/main/matlab/point_patient.m
+++ deeplearning/src/main/matlab/point_patient.m
@@ -1,10 +1,7 @@
-%File of points we would like to create
 file_points = fopen('100points.in','w');
 
-%Number of points printed
 fprintf(file_points,'%i\n', size(P,1));
 
-%Get all the points and print on each line
 for i = 1:size(P,1)
     fprintf(file_points,'%f ', P(i,1:end-1));
     fprintf(file_points,'%f\n', P(i,end));
@@ -12,12 +9,9 @@ end
 
 fclose(file_points);
 
-%File of points we would like to create
 file_patients = fopen('geneact.in','w');
 
-%Print the number of labels as first line
 fprintf(file_patients, '%i\n', size(CancerTypeList,1));
-%Get all the gene information into the file
 for i = 1:size(GE,1)
     fprintf(file_patients,'%f ', GE(i,1:end-1));
     fprintf(file_patients,'%f\n', GE(i,end));
