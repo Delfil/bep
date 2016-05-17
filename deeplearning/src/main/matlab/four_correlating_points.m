049d00c62a6d4ffe271731581d400daed4da33fa
paulbakker65 <P.Bakker-2@student.tudelft.nl>
2016-05-17 14:53:09 +0200

    New test data, now without -1 as label

--- deeplearning/src/main/matlab/four_correlating_points.m
+++ deeplearning/src/main/matlab/four_correlating_points.m
@@ -7,9 +7,9 @@ column = absCorr(:,point);
 fourpoints = sortedIndex(1:4);
 avg_act = mean(Gene_Expression(:,fourpoints),2);
 
-dataGen(Gene_Expression(:,sortedIndex(1)), CancerTypeIndex-1, 'name', 'gene1', 'height', 1, 'width', 1);
-dataGen(Gene_Expression(:,sortedIndex(2)), CancerTypeIndex-1, 'name', 'gene2', 'height', 1, 'width', 1);
-dataGen(Gene_Expression(:,sortedIndex(3)), CancerTypeIndex-1, 'name', 'gene3', 'height', 1, 'width', 1);
-dataGen(Gene_Expression(:,sortedIndex(4)), CancerTypeIndex-1, 'name', 'gene4', 'height', 1, 'width', 1);
-dataGen(avg_act, CancerTypeIndex-1, 'name', 'geneAVG', 'height', 1, 'width', 1);
+dataGen(Gene_Expression(:,sortedIndex(1)), CancerTypeIndex, 'name', 'gene1', 'height', 1, 'width', 1);
+dataGen(Gene_Expression(:,sortedIndex(2)), CancerTypeIndex, 'name', 'gene2', 'height', 1, 'width', 1);
+dataGen(Gene_Expression(:,sortedIndex(3)), CancerTypeIndex, 'name', 'gene3', 'height', 1, 'width', 1);
+dataGen(Gene_Expression(:,sortedIndex(4)), CancerTypeIndex, 'name', 'gene4', 'height', 1, 'width', 1);
+dataGen(avg_act, CancerTypeIndex, 'name', 'geneAVG', 'height', 1, 'width', 1);
 
