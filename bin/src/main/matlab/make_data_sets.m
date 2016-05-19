5a5e548ed6b84ec2c8613be3cc875211294171c9
paulbakker65 <P.Bakker-2@student.tudelft.nl>
2016-05-19 16:59:00 +0200

    Removed bin

--- bin/src/main/matlab/make_data_sets.m
+++
@@ -1,7 +0,0 @@
-function make_data_sets(N_test, N_train, N_genes, Data, Labels, file_name)
-
-    if N_test + N_train < size(Data,1) && N_genes < size(Data,2) && size(Labels,1) == size(Data,1)
-        sample_data(N_test, N_genes, Data, Labels, [file_name '.test']);
-        sample_data(N_train, N_genes, Data, Labels, [file_name '.train']);
-    end
-end
