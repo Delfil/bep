d540c3b1d1487999040fd6cbb2002a060fe712c7
paulbakker65 <P.Bakker-2@student.tudelft.nl>
2016-05-19 17:21:56 +0200

    Removed merge conflict point_patients

--- src/main/matlab/point_patient.m
+++ src/main/matlab/point_patient.m
@@ -1,9 +1,6 @@
-<<<<<<< HEAD:src/main/matlab/point_patient.m
-file_points = fopen('100points.in','w');
-=======
 %File of points we would like to create
 file_points = fopen('point.in','w');
->>>>>>> origin:src/main/matlab/point_patient.m
+
 
 %Print the number of points on the first line
 fprintf(file_points,'%i\n', size(P,1));
@@ -16,16 +13,11 @@ end
 
 fclose(file_points);
 
-<<<<<<< HEAD:src/main/matlab/point_patient.m
-file_patients = fopen('geneact.in','w');
-
-fprintf(file_patients, '%i\n', size(CancerTypeList,1));
-=======
 %File of gene activation we would like to create
 file_patients = fopen('patients.in','w');
 
 %Gene activation per patient per row.
->>>>>>> origin:src/main/matlab/point_patient.m
+
 for i = 1:size(GE,1)
     fprintf(file_patients,'%f ', GE(i,1:end-1));
     fprintf(file_patients,'%f\n', GE(i,end));
