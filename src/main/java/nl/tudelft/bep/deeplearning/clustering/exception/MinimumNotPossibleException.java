ab8e25c75bfe8ee897dabe9945b2291b14455529
paulbakker65 <P.Bakker-2@student.tudelft.nl>
2016-05-23 10:55:49 +0200

    serialVersion removed

--- src/main/java/nl/tudelft/bep/deeplearning/clustering/exception/MinimumNotPossibleException.java
+++ src/main/java/nl/tudelft/bep/deeplearning/clustering/exception/MinimumNotPossibleException.java
@@ -2,9 +2,4 @@ package nl.tudelft.bep.deeplearning.clustering.exception;
 
 public class MinimumNotPossibleException extends Exception {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
 }
