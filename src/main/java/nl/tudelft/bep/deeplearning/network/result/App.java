f0e57aad718ffc3a1429b4e6f264a755ac367838
paulbakker65 <P.Bakker-2@student.tudelft.nl>
2016-05-25 10:39:02 +0200

    Three networks 6x6 added to App.class

--- src/main/java/nl/tudelft/bep/deeplearning/network/result/App.java
+++ src/main/java/nl/tudelft/bep/deeplearning/network/result/App.java
@@ -15,6 +15,26 @@ public class App {
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		ResultUtil.generateCSV(10, new ComputeAverageAccuracyFiller(20), "results");
 		CNN.BuildExampleCNN(
+				new ConvolutionLayer.Builder(6, 6).nIn(1).stride(1, 1).nOut(20).activation("identity").build(),
+				new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX).kernelSize(2, 2).stride(2, 2).build(),
+				new DenseLayer.Builder().activation("relu").nOut(20).build(),
+				new OutputLayer.Builder(LossFunction.NEGATIVELOGLIKELIHOOD).nOut(5).activation("softmax").build())
+				.backprop(true).pretrain(false).finish("6x6 kernelsize Max pooling");
+
+		CNN.BuildExampleCNN(
+				new ConvolutionLayer.Builder(6, 6).nIn(1).stride(1, 1).nOut(20).activation("identity").build(),
+				new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.AVG).kernelSize(2, 2).stride(2, 2).build(),
+				new DenseLayer.Builder().activation("relu").nOut(20).build(),
+				new OutputLayer.Builder(LossFunction.NEGATIVELOGLIKELIHOOD).nOut(5).activation("softmax").build())
+				.backprop(true).pretrain(false).finish("6x6 kernelsize Avg pooling");
+		
+		CNN.BuildExampleCNN(
+				new ConvolutionLayer.Builder(6, 6).nIn(1).stride(2, 2).nOut(20).activation("identity").build(),
+				new DenseLayer.Builder().activation("relu").nOut(20).build(),
+				new OutputLayer.Builder(LossFunction.NEGATIVELOGLIKELIHOOD).nOut(5).activation("softmax").build())
+				.backprop(true).pretrain(false).finish("6x6 kernelsize No pooling with stride compensation");
+		
+		CNN.BuildExampleCNN(
 				new ConvolutionLayer.Builder(5, 5).nIn(1).stride(1, 1).nOut(20).activation("identity").build(),
 				new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX).kernelSize(2, 2).stride(2, 2).build(),
 				new DenseLayer.Builder().activation("relu").nOut(20).build(),
