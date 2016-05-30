ea77222a1117529e1d47613ac46aa29325982427
S. Smulders <s.smulders@student.tudelft.nl>
2016-05-30 12:13:28 +0200

    Baseline test

--- src/main/java/nl/tudelft/bep/deeplearning/network/App.java
+++ src/main/java/nl/tudelft/bep/deeplearning/network/App.java
@@ -1,5 +1,11 @@
 package nl.tudelft.bep.deeplearning.network;
 
+import org.deeplearning4j.nn.conf.layers.ConvolutionLayer;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.nd4j.linalg.lossfunctions.LossFunctions.LossFunction;
+
+import nl.tudelft.bep.deeplearning.network.builder.CNN;
 import nl.tudelft.bep.deeplearning.network.result.ResultUtil;
 import nl.tudelft.bep.deeplearning.network.result.csv.ComputeAverageAccuracyFiller;
 
@@ -23,6 +29,31 @@ public final class App {
 	 *            the file name to save to
 	 */
 	public static void main(final String[] args) {
+
+		CNN.buildExampleCNN(
+				new ConvolutionLayer.Builder(1, 1).nIn(1).stride(1, 1).nOut(1).activation("identity").build(),
+				new DenseLayer.Builder().activation("relu").nOut(100).build(),
+				new OutputLayer.Builder(LossFunction.NEGATIVELOGLIKELIHOOD).nOut(5).activation("softmax").build())
+				.backprop(true).pretrain(false).finish("1x1");
+
+		CNN.buildExampleCNN(
+				new ConvolutionLayer.Builder(4, 1).nIn(1).stride(1, 1).nOut(20).activation("identity").build(),
+				new DenseLayer.Builder().activation("relu").nOut(100).build(),
+				new OutputLayer.Builder(LossFunction.NEGATIVELOGLIKELIHOOD).nOut(5).activation("softmax").build())
+				.backprop(true).pretrain(false).finish("4x1");
+
+		CNN.buildExampleCNN(
+				new ConvolutionLayer.Builder(9, 1).nIn(1).stride(1, 1).nOut(20).activation("identity").build(),
+				new DenseLayer.Builder().activation("relu").nOut(100).build(),
+				new OutputLayer.Builder(LossFunction.NEGATIVELOGLIKELIHOOD).nOut(5).activation("softmax").build())
+				.backprop(true).pretrain(false).finish("9x1");
+
+		CNN.buildExampleCNN(
+				new ConvolutionLayer.Builder(16, 1).nIn(1).stride(1, 1).nOut(20).activation("identity").build(),
+				new DenseLayer.Builder().activation("relu").nOut(100).build(),
+				new OutputLayer.Builder(LossFunction.NEGATIVELOGLIKELIHOOD).nOut(5).activation("softmax").build())
+				.backprop(true).pretrain(false).finish("16x1");
+
 		run((args.length >= 1 && isInteger(args[0])) ? Integer.parseInt(args[0]) : DEFAULT_EPOCHS,
 				(args.length >= 2 && isInteger(args[1])) ? Integer.parseInt(args[1]) : DEFAULT_ITERATIONS,
 				(args.length >= 3 ? args[2] : DEFAULT_FILE_NAME));
