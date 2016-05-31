6b3730ab4f0f19b820578e188af2d38cb4448608
S. Smulders <s.smulders@student.tudelft.nl>
2016-05-31 14:33:03 +0200

    Network added for experiment

--- src/main/java/nl/tudelft/bep/deeplearning/network/App.java
+++ src/main/java/nl/tudelft/bep/deeplearning/network/App.java
@@ -1,5 +1,10 @@
 package nl.tudelft.bep.deeplearning.network;
 
+import org.deeplearning4j.nn.conf.layers.ConvolutionLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.nd4j.linalg.lossfunctions.LossFunctions.LossFunction;
+
+import nl.tudelft.bep.deeplearning.network.builder.CNN;
 import nl.tudelft.bep.deeplearning.network.result.ResultUtil;
 import nl.tudelft.bep.deeplearning.network.result.csv.ComputeAverageAccuracyFiller;
 
@@ -23,6 +28,11 @@ public final class App {
 	 *            the file name to save to
 	 */
 	public static void main(final String[] args) {
+		CNN.buildExampleCNN(
+				new ConvolutionLayer.Builder(1, 1).nIn(1).stride(1, 1).nOut(1).activation("identity").build(),
+				new OutputLayer.Builder(LossFunction.NEGATIVELOGLIKELIHOOD).nOut(5).activation("softmax").build())
+				.backprop(true).pretrain(false).finish("Test");
+
 		run((args.length >= 1 && isInteger(args[0])) ? Integer.parseInt(args[0]) : DEFAULT_EPOCHS,
 				(args.length >= 2 && isInteger(args[1])) ? Integer.parseInt(args[1]) : DEFAULT_ITERATIONS,
 				(args.length >= 3 ? args[2] : DEFAULT_FILE_NAME));
