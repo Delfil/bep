e0480d655fccd2743dba0b54ebad00806f847fd8
Berend Ottervanger <berendottervanger@gmail.com>
2016-06-02 12:30:28 +0200

    added inv corr tsne 1d

--- src/main/matlab/IME_inv_corrs_100_genes.m
+++ src/main/matlab/IME_inv_corrs_100_genes.m
@@ -1,11 +1,17 @@
-%select the 100 genes with the highest standard deviation calculate
-%correlations and run tsne on the inverse. then make a matrix from that and
-%
-
-if ~exist('Gene_Expression','var') || ~exist('CancerTypeIndex','var')
-    disp('Loading the database.');
-    load('GE.mat');
-end
+function IME_inv_corrs_100_genes(in_matlab_folder)
+    %select the 100 genes with the highest standard deviation calculate
+    %correlations and run tsne on the inverse. then make a matrix from that and
+    %
+    name = 'IME_inv_corrs_100_genes';
+    
+    if ~exist('Gene_Expression','var') || ~exist('CancerTypeIndex','var')
+        disp('Loading the database.');
+        load('GE.mat');
+    end
+    
+    if ~exist('in_matlab_folder', 'var')
+        in_matlab_folder = false;
+    end
 
     GE = selectNGenes(100,Gene_Expression);
     corrs = corr(GE);
@@ -19,4 +25,16 @@ end
     data = GEt(:,genes+1);
     clear GEt;
     
-    dataGen(data, CancerTypeIndex, 'name', 'IME_inv_corrs_100_genes', 'height', size(M,1), 'width', size(M,2));
+    %navigate to right folder
+    if in_matlab_folder
+        path = sprintf('../../../datasets/%s/', name);
+    else
+        path = sprintf('%s/', name);
+    end
+
+    mkdir(path);
+    
+    dataGen(data, CancerTypeIndex,...
+        'name', [path, name],...
+        'height', size(M,1), 'width', size(M,2));
+end
---
+++ src/main/matlab/TSNE_1D_inv_corrs_100_genes.m
@@ -0,0 +1,42 @@
+function TSNE_1D_inv_corrs_100_genes(in_matlab_folder)
+    %select the 100 genes with the highest standard deviation.
+    %calculate correlations and run tsne on the inverse.
+    %run 1d t-sne
+    name = 'TSNE_1D_inv_corrs_100_genes';
+
+    if ~exist('Gene_Expression','var') || ~exist('CancerTypeIndex','var')
+        disp('Loading the database.');
+        load('GE.mat');
+    end
+    
+    if ~exist('in_matlab_folder', 'var')
+        in_matlab_folder = false;
+    end
+
+    % take the 100 genes with highest standard deviation
+    GE = selectNGenes(100,Gene_Expression);
+    
+    % calculate correlations
+    corrs = corr(GE);
+    
+    % invert the correlations
+    inverseCorrs = -1*corrs;
+    
+    %reduce to one dimension
+    P = tsne_p(inverseCorrs, [], 1);
+    
+    %sort the points
+    [~,ind] = sort(P);
+    
+    %navigate to right folder
+    if in_matlab_folder
+        path = sprintf('../../../datasets/%s/', name);
+    else
+        path = sprintf('%s/', name);
+    end
+
+    mkdir(path);
+    
+    dataGen(GE(:,ind), CancerTypeIndex, 'width', 1, 'height', numel(ind),...
+        'name', [path, name])
+end
